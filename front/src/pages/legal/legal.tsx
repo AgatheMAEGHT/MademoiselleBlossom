3acd496bd23464dd00ca9821f41ad4027267221b
Agathe Maeght <agathe.maeght@epita.fr>
2024-05-12 14:30:29 +0200

    feat: legal

---
+++ front/src/pages/legal/legal.tsx
@@ -0,0 +1,88 @@
+import './legal.css';
+
+function Legal() {
+    return (
+        <div id="legal">
+            <h2>Mentions Légales</h2>
+            <h3>Informations légales</h3>
+            <p className='contact-emma-text'>Le site mademoiselleblossom.fr est édité par la société Mademoiselle Blossom</p>
+            <p className='contact-emma-text'>Appartenant à : Emma BOULABAS<br />
+                SIRET : 97794890000013<br />
+                Téléphone : <i className='contact-info-separation'>numéro </i>06 <i className='contact-info-separation'>puis</i>16 <i className='contact-info-separation'>puis</i>28 <i className='contact-info-separation'>puis</i>28 <i className='contact-info-separation'>puis</i>83<br />
+                E-mail : ma<i className='contact-info-separation'>puis</i>demoiselle<i className='contact-info-separation'>puis</i>.blossom<i className='contact-info-separation'>puis</i>34<i className='contact-info-separation'>puis</i>@<i className='contact-info-separation'>puis</i>g<i className='contact-info-separation'>puis</i>mail<i className='contact-info-separation'>puis</i>.<i className='contact-info-separation'>puis</i>com<br />
+                Directrice de la publication : Emma BOULABAS<br />
+                Hébergeur : Amazon Web Services, Inc. P.O. Box 81226 Seattle, WA 98108-1226 USA http://aws.amazon.com</p>
+
+            <h3>Propriété intellectuelle et contrefaçons</h3>
+            <p className='contact-emma-text'>Mademoiselle Blossom détient les droits de propriété intellectuelle sur tous les éléments accessibles sur le site, tels que les textes, images, graphismes, logos, icônes, etc.</p>
+            <p className='contact-emma-text'>Toute reproduction, représentation, modification, publication ou adaptation de tout ou partie des éléments du site est strictement interdite sans l'autorisation écrite préalable de Mademoiselle Blossom. Toute utilisation non autorisée du site ou de ses éléments constitue une contrefaçon et sera poursuivie conformément aux dispositions du Code de Propriété Intellectuelle.</p>
+
+            <h3>Politique de confidentialité</h3>
+            <p className='contact-emma-text'>Cette politique a pour objectif d'informer les utilisateurs sur les données personnelles collectées lors de l'utilisation de notre site, ainsi que sur les points suivants, le cas échéant :</p>
+
+            <ul className='contact-emma-info'>
+                <li>Les types de données collectées</li>
+                <li>L'utilisation de ces données</li>
+                <li>Les personnes ayant accès à ces données</li>
+                <li>Les droits des utilisateurs</li>
+            </ul>
+
+            <h3>Conformité au RGPD</h3>
+            <p className='contact-emma-text'>Cette politique est conforme au Règlement général sur la protection des données (RGPD). Les données personnelles doivent être traitées selon les principes suivants :</p>
+            <ul className='contact-emma-info'>
+                <li>Licéité, loyauté et transparence dans le traitement des données</li>
+                <li>Collecte et traitement limités aux finalités spécifiques</li>
+                <li>Minimisation des données collectées</li>
+                <li>Exactitude des données et mise à jour si nécessaire</li>
+                <li>Conservation limitée dans le temps</li>
+                <li>Sécurité des données assurée</li>
+            </ul>
+            <p className='contact-emma-text'>Le traitement des données est licite s'il répond à certaines conditions spécifiées, telles que le consentement de la personne concernée ou l'exécution d'un contrat.</p>
+
+            <h3>Conformité à la CCPA</h3>
+            <p className='contact-emma-text'>Pour les résidents de Californie, cette politique vise à respecter la California Consumer Privacy Act (CCPA). En cas de conflit entre cette politique et la CCPA, la législation de l'État prévaudra.</p>
+
+            <h3>Consentement</h3>
+            <p className='contact-emma-text'>En utilisant notre site, les utilisateurs consentent aux conditions de cette politique ainsi qu'à la collecte, l'utilisation et la conservation des données personnelles décrites ici.</p>
+
+            <h3>Données personnelles collectées</h3>
+            <p className='contact-emma-text'>Les données collectées sont utilisées uniquement aux fins précisées dans cette politique :</p>
+            <ul className='contact-emma-info'>
+                <li>Création d'un compte</li>
+            </ul>
+            <p className='contact-emma-text'>Les données ne seront pas utilisées à d'autres fins sans le consentement de l'utilisateur.</p>
+            <p className='contact-emma-text'>Les données collectées ne seront pas partagées avec des tiers sans le consentement de l'utilisateur, sauf dans les cas prévus par la loi.</p>
+            <p className='contact-emma-text'>Les données recueillies sont les suivantes :</p>
+            <ul className='contact-emma-info'>
+                <li>Nom</li>
+                <li>Prénom</li>
+                <li>Adresse e-mail</li>
+                <li>Numéro de téléphone</li>
+            </ul>
+
+            <h3>Conservation et protection des données personnelles</h3>
+            <p className='contact-emma-text'>Nous conservons les données aussi longtemps que nécessaire pour atteindre les finalités pour lesquelles elles ont été collectées.</p>
+            <p className='contact-emma-text'>Nous prenons des mesures pour sécuriser les données, mais il subsiste un risque résiduel. Nous ne sommes pas responsables des politiques de confidentialité des sites tiers.</p>
+            <p className='contact-emma-text'>Nous nous engageons à ne pas vendre ou partager vos données avec des tiers, sauf dans les cas suivants :</p>
+            <ul>
+                <li>Si la loi l'exige</li>
+                <li>Si elle est requise pour toute procédure judiciaire</li>
+                <li>Pour prouver ou protéger nos droits légaux</li>
+                <li>À des acheteurs ou des acheteurs potentiels de cette société dans le cas où nous cherchons à la vendre la société</li>
+            </ul>
+
+            <h3>Accès aux données personnelles</h3>
+            <p className='contact-emma-text'>Les utilisateurs peuvent, en vertu du RGPD, accéder à leurs données personnelles, les rectifier ou les supprimer en me contactant à l'adresse suivante : ma<i className='contact-info-separation'>puis</i>demoiselle<i className='contact-info-separation'>puis</i>.blossom<i className='contact-info-separation'>puis</i>34<i className='contact-info-separation'>puis</i>@<i className='contact-info-separation'>puis</i>g<i className='contact-info-separation'>puis</i>mail<i className='contact-info-separation'>puis</i>.<i className='contact-info-separation'>puis</i>com.</p>
+
+            <h3>Mise à jour des Mentions Légales</h3>
+            <p className='contact-emma-text'>La société éditrice se réserve le droit de mettre à jour les Mentions Légales du site à tout moment et sans préavis. Nous vous invitons donc à consulter régulièrement les dernières Conditions Générales d'Utilisation en vigueur lors de votre visite sur le site.</p>
+
+            <h3>Contact</h3>
+            <p className='contact-emma-text'>Pour toute question ou demande d'information concernant les Mentions Légales du site, veuillez nous contacter à l'adresse suivante : ma<i className='contact-info-separation'>puis</i>demoiselle<i className='contact-info-separation'>puis</i>.<i className='contact-info-separation'>puis</i>blossom<i className='contact-info-separation'>puis</i>34<i className='contact-info-separation'>puis</i>@<i className='contact-info-separation'>puis</i>g<i className='contact-info-separation'>puis</i>mail<i className='contact-info-separation'>puis</i>.<i className='contact-info-separation'>puis</i>com.</p>
+
+            <p className='contact-emma-text'>Les Mentions Légales ont été mises à jour le 12 mai 2024.</p>
+        </div>
+    );
+}
+
+export default Legal;
